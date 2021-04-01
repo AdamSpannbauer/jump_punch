0aa0d29d5f4210c3f161700b8a07244ceac61e9f
AlexShepherd28 <ashepherd2821@gmail.com>
2021-04-01 14:03:09 -0400

    add game and client

---
+++ game.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self, players):
+        self.players = players
+        self.HUD = None
+
+    def update(self):
+        pass
+
+    def draw_HUD(self):
+        pass
+
+    def draw(self):
+        pass
