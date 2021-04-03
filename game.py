19dc5d8a50ead0632f41e7b35aba0c83bf652374
AlexShepherd28 <ashepherd2821@gmail.com>
2021-04-03 05:17:40 -0400

    add some initial pygame stuff

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class Game:
     def __init__(self, players):
         self.players = players
@@ -11,3 +14,37 @@ class Game:
 
     def draw(self):
         pass
+
+
+BLACK = (0, 0, 0)
+GRAY = (100, 100, 100)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+fps = 60
+game_over = False
+winner = None
+display_size = (600, 600)
+
+pygame.init()
+
+
+def _quit():
+    pygame.display.quit()
+    pygame.quit()
+    print(f"GAME OVER\nWinner: {winner}")
+    quit()
+
+
+screen = pygame.display.set_mode(display_size)
+pygame.display.set_caption("FIGHT!")
+
+# YEP
+clock = pygame.time.Clock()
+
+while game_over is False:
+    clock.tick(fps)
+    screen.fill(BLACK)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            _quit()
