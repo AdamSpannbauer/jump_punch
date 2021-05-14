80f58bc219186ea8cf445cd6941cb213d31de3df
AdamSpannbauer <spannbaueradam@gmail.com>
2021-05-14 06:18:34 -0400

    bring in hypotheses

---
+++ network_client.py
@@ -0,0 +1,26 @@
+import socket
+from network_utils import get_host_name, PORT, FORMAT
+
+SERVER = get_host_name()
+
+
+class NetworkClient:
+    def __init__(self, addr, port):
+        self.addr = addr
+        self.port = port
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send(self, data):
+        self.connection.sendto(data.encode(FORMAT), (SERVER, PORT))
+
+    def recv(self):
+        data, addr = self.connection.recvfrom(1024)
+
+        print(f"response from: {addr}")
+        print(f"received: {data.decode(FORMAT)}")
+
+
+if __name__ == "__main__":
+    client = NetworkClient(SERVER, PORT)
+    client.send("whats up")
+    client.recv()
--- player.py
+++ player.py
@@ -1,10 +1,17 @@
+from uuid import uuid4
 import pygame
+from network_utils import encode_network_data, decode_network_data
 
 
 class Player:
-    def __init__(self, x, y, color=(200, 50, 200)):
+    def __init__(self, x, y, color=(200, 50, 200), player_id=None):
         self.x = x
         self.y = y
+
+        self.id = player_id
+        if not self.id:
+            self.id = self._gen_player_id()
+
         self.color = color
         self.speed = 1
         self.gravity = 1
@@ -12,13 +19,24 @@ class Player:
         self.hitboxes = []
         self.hurtboxes = []
         self.health = 1
-        self.network_client = None
+
+    @staticmethod
+    def _gen_player_id():
+        return str(uuid4())
+
+    def encode_network_data(self):
+        return encode_network_data(self.id, self.x, self.y)
+
+    def decode_network_data(self, data):
+        _, x, y = decode_network_data(data)
+        self.x = x
+        self.y = y
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, 50, 50))
 
     def update(self):
-        pass
+        self.move()
 
     def jump(self):
         pass
